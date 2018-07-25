c3e078a1e1f23ef47fa6a7231350d16424a3e565
Nick Lee <nick@nicklee.co.uk>
2018-07-25 17:08:09 +0100

    Homepage components

--- site/patterns/01-molecules/slideshow/slideshow.js
+++ site/patterns/01-molecules/slideshow/slideshow.js
@@ -1,25 +1,39 @@
-// import $ from 'jquery';
-// import 'owl.carousel';
-//
-// function Slideshow() {
-//
-//     // Attach Carousel to dom node, with options
-//     $('.js-slideshow').owlCarousel({
-//         items:1,
-//         singleitem: true,
-//         nav:true,
-//         autoHeight:true,
-//         dots:true
-//     });
-//
-//     $('.js-slideshow--no-nav').owlCarousel({
-//         items:1,
-//         singleitem: true,
-//         nav:false,
-//         autoHeight:true,
-//         dots:true
-//     });
-//
-// }
-//
-// Slideshow();
+import Glide from '@glidejs/glide';
+
+class Slideshow {
+
+    constructor( ) {
+        this.slideshowContainer =  '.js-slideshow';
+
+        this.launchSlideshow(); 
+    }
+
+    launchSlideshow() {
+
+        new Glide(this.slideshowContainer, {
+            type: 'slider',
+            startAt: 0,
+            perView: 2,
+            gap: 28,
+            peek: {
+                before: 0,
+                after: 200
+            },
+            breakpoints: {
+                1024: {
+                    perView: 2
+                },
+                600: {
+                    perView: 1
+                }
+            }
+        }).mount();
+
+    }
+
+}
+
+
+[].forEach.call(document.getElementsByClassName('js-slideshow'), function(el) {
+    new Slideshow(el);
+});
