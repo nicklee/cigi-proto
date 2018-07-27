ce4635a0bd09a8114c839a771373d44966d38a6d
Nick Lee <nick@nicklee.co.uk>
2018-07-27 13:00:13 +0100

    Latest grid feed

--- site/patterns/00-atoms/modal/modal.js
+++ site/patterns/00-atoms/modal/modal.js
@@ -1,31 +1,31 @@
-import lightGallery from 'lightgallery.js';
-import 'lg-video.js';
-
-class lightGalleryModal {
-
-    constructor( ) {
-        this.launchModal();
-    }
-
-    launchModal() {
-
-        new lightGallery(document.getElementById('js-modal'), {
-            download: false,
-            counter: false,
-            controls: false,
-            selector: '.js-modal-item',
-            youtubePlayerParams: {
-                modestbranding: 1,
-                showinfo: 0,
-                rel: 0,
-                controls: 0
-            }
-        }).mount();
-
-    }
-
-}
-
-[].forEach.call(document.getElementById('js-modal'), function() {
-    new lightGalleryModal();
-});
+// import lightGallery from 'lightgallery.js';
+// import 'lg-video.js';
+//
+// class lightGalleryModal {
+//
+//     constructor( ) {
+//         this.launchModal();
+//     }
+//
+//     launchModal() {
+//
+//         new lightGallery(document.getElementById('js-modal'), {
+//             download: false,
+//             counter: false,
+//             controls: false,
+//             selector: '.js-modal-item',
+//             youtubePlayerParams: {
+//                 modestbranding: 1,
+//                 showinfo: 0,
+//                 rel: 0,
+//                 controls: 0
+//             }
+//         }).mount();
+//
+//     }
+//
+// }
+//
+// [].forEach.call(document.getElementById('js-modal'), function() {
+//     new lightGalleryModal();
+// });
