0b63b13391cf617687c9d564f85a4612f4d3f59e
nicklee <nick@nicklee.co.uk>
2019-01-08 10:13:23 +0000

    Publications pages

--- site/patterns/01-molecules/carousel/carousel.js
+++ site/patterns/01-molecules/carousel/carousel.js
@@ -19,6 +19,30 @@ class Carousel {
 
 }
 
+class CarouselLong {
+
+    constructor(node) {
+        this.launchcarousel(node);
+    }
+
+    launchcarousel(node) {
+
+        new Glide(node, {
+            type: 'slider',
+            startAt: 0,
+            perView: 3,
+            gap: 28
+        }).mount();
+
+    }
+
+}
+
 [].forEach.call(document.getElementsByClassName('js-carousel'), function(el) {
     new Carousel(el);
+});
+
+
+[].forEach.call(document.getElementsByClassName('js-carousel-long'), function(el) {
+    new CarouselLong(el);
 });
